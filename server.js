75fbe3a64b4859191ce26d1130544ccd396b7042
Jmo5896 <jmo5896@moore-clan.net>
2018-07-22 12:47:29 -0600

    turned on heroku server thing

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Add routes, both API and view
 app.use("/api", router);
 
